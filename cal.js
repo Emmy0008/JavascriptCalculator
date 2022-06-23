2843fbdd53f146917d0fc9bd9a7fd053514b452e
Emmy0008 <emmy000eight@gmail.com>
2022-06-23 23:19:24 +0100

    Basic Arithmetic Calculator work

---
+++ cal.js
@@ -0,0 +1,24 @@
+const operator = prompt("Enter operator: +, -, *, or /");
+
+const number1 = parseInt(prompt("Enter First Number: "));
+const number2 = parseInt(prompt("Enter Second Number: "));
+
+let result;
+
+if(operator == "+"){
+    result = number1 + number2;
+}
+else if (operator == "-") {
+    result = number1 - number2;
+}
+else if (operator == "*") {
+    result = number1 * number2;
+} else if(operator == "/"){
+    result = number1 / number2;
+}
+else {
+    document.write("invalid function");
+}
+
+alert(result);
+// console.log(`${number1} ${operator} ${number2} = ${result}`);
